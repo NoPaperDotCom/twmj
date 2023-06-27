e5d9d4e5505969f8987d187235b6e0213a2534eb
NoPaperDotCom <130741956+NoPaperDotCom@users.noreply.github.com>
2023-06-28 01:02:48 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,13 +1,13 @@
 /** @type {import('next').NextConfig} */
 const { i18n } = require("./next-i18next.config");
-const keys = require("./private.key.js");
+// const keys = require("./private.key.js");
 
 const nextConfig = {
   // reactStrictMode: true,
   env: {
     PARSE_SERVER_URL: "https://twmj.b4a.io/",
-    PARSE_WEBSOCKET_URL: "wss://twmj.b4a.io/",
-    ...keys
+    PARSE_WEBSOCKET_URL: "wss://twmj.b4a.io/"
+    // ...keys
   },
   i18n
 };
