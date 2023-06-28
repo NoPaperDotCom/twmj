1cb0576af40f3d681219aed588c2926ffd745522
NoPaperDotCom <130741956+NoPaperDotCom@users.noreply.github.com>
2023-06-28 17:37:05 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
 const { i18n } = require("./next-i18next.config");
-const keys = require("./private.key.js");
 
 const nextConfig = {
   // reactStrictMode: true,
@@ -8,8 +7,7 @@ const nextConfig = {
     PARSE_SERVER_URL: "https://twmj.b4a.io/",
     PARSE_WEBSOCKET_URL: "wss://twmj.b4a.io/",
     PARSE_APPLICATION_ID: "w5kqvzuSTSoxS98GxQgULuGBVfZcpXLqrdCRAiWV",
-    PARSE_JAVASCRIPT_KEY: "ol2lcJihAEOxsNf04o8Gb6YpecSJ3MHHk80XC1vy",
-    ...keys
+    PARSE_JAVASCRIPT_KEY: "ol2lcJihAEOxsNf04o8Gb6YpecSJ3MHHk80XC1vy"
   },
   i18n
 };
