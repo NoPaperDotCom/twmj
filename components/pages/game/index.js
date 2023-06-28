d66a731ad4514877d67a57ae99142e45aff60d93
NoPaperDotCom <130741956+NoPaperDotCom@users.noreply.github.com>
2023-06-28 17:47:14 +0800

    Update index.js

--- components/pages/game/index.js
+++ components/pages/game/index.js
@@ -28,7 +28,7 @@ import AppError from "@/utils/error";
 const _callParseCloudFunction = async (funcName, params, router, setSetting) => {
   try {
     setOverlayDisplay("loading-popup", true);
-    const { status, ...others } = await callParseMethod(funcName, params);
+    const { status, ...others } = await Parse.Cloud.run(funcName, params);
     if (status === "error") { throw new AppError({ text: "parse-error", status: 500, message: others.error }); }
 
     setOverlayDisplay("loading-popup", false);
